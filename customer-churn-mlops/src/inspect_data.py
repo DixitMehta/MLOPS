c6a9c4d4acfc03d0e265611f9ef53c835a8a833d
Dixit <Dixit@gmail.com>
2026-09-22 19:51:39 +0530

    Created inspect_data.py

--- customer-churn-mlops/src/inspect_data.py
+++ customer-churn-mlops/src/inspect_data.py
@@ -1,4 +1,27 @@
 import pandas as pd
 
 #Load dataset
-df = pd.read_csv("data/raw/Customer.csv")
+df = pd.read_csv("../data/raw/Customer.csv")
+
+#Display first 5 rows
+print("First 5 rows")
+print(df.head())
+
+#Display dataset information
+print("\nDataset information:")
+print(df.info())
+
+#Check for missing values
+print("\nMissing values:b")
+print(df.isnull().sum())
+
+#Display basic statistics
+print('\n Statistical Summary:')
+print(df.describe())
+
+# Display number of records
+print('\n Total records: ', len(df))
+
+# Display columns
+print('Colums:')
+print(df.columns.tolist())
